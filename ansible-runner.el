ef85874b7a9f17e2079bc47afea0c460313b3716
Antti Salminen <antti.salminen@gmail.com>
2015-09-30 00:22:06 +0300

    Make the command more customizable.

--- ansible-runner.el
+++ ansible-runner.el
@@ -7,20 +7,21 @@
 (defvar ansiblerunner-hosts "all")
 (defvar ansiblerunner-playbook "/home/anttis/projektit/ansible/kifi-playbooks/site.yml")
 (defvar ansiblerunner-inventory "/home/anttis/projektit/vagrant/vagrant_ansible_dev/inventory")
+(defvar ansiblerunner-ansible-command "ansible-playbook -D")
 
 (defun ansiblerunner-compile (start end)
 (interactive "r")
 (let ((playbook (make-temp-file "ansible")))
 (write-region (concat "- hosts: all\n\n  tasks:\n") nil playbook)
 (write-region start end playbook t)
-(compile (concat "ansible-playbook -i /home/anttis/projektit/vagrant/vagrant_ansible_dev/inventory -l monihaku " playbook t)))
+(compile (concat ansiblerunner-ansible-command " -i /home/anttis/projektit/vagrant/vagrant_ansible_dev/inventory -l monihaku " playbook t)))
 )
 
 (defun ansiblerunner-run ()
 (interactive)
 (setq default-directory "/home/anttis/projektit/vagrant/vagrant_ansible_dev")
 (let ((default-directory "/home/anttis/projektit/vagrant/vagrant_ansible_dev"))
-(compile (concat "ansible-playbook -i " ansiblerunner-inventory " -l "
+(compile (concat ansiblerunner-ansible-command " -i " ansiblerunner-inventory " -l "
 ansiblerunner-hosts " --start-at-task '" (ansiblerunner-current-task-name) "' --step "
  ansiblerunner-playbook ) t))
 )
